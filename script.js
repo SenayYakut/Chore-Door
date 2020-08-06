a15f198034f45800dcf42713f252de532cfc97c9
Senay Yakut <senaykt@gmail.com>
2020-08-06 16:58:10 -0700

    onclick event

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let doorImage1 = document.getElementById("door1");
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
-doorImage1.onclick = () => {
+doorImage1.onclick = function(){
   doorImage1.src = botDoorPath;
 } 
 
