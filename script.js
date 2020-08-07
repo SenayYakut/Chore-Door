3619c3572b87a5d303fa5810294556aa229d3d44
Senay Yakut <senaykt@gmail.com>
2020-08-06 18:09:51 -0700

    When you click each door , you will see another image

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let doorImage1 = document.getElementById("door1");
-const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
-doorImage1.onclick = function(){
-  doorImage1.src = botDoorPath;
-} 
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+
+let doorImage1 = document.getElementById("door1");
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3");
+
+doorImage1.onclick = () => {
+  doorImage1.src = botDoorPath;
+}; 
+
+doorImage2.onclick = () =>{
+  doorImage2.src = beachDoorPath;
+};
+doorImage3.onclick = () =>{
+  doorImage3.src = spaceDoorPath;
+};
 
